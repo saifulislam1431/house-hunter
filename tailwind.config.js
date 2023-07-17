accf4cdef3b15fc503e6ecaf484df16743aa1f63
Saiful Islam <sakibbhaiya143@gmail.com>
2023-07-17 14:16:38 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#00DF75",
+
+          "secondary": "#00BB9D",
+
+          "accent": "#0094A3",
+
+          "neutral": "#2F4858",
+
+          "base-100": "#fcfcfd",
+
+          "info": "#00B4E7",
+
+          "success": "#17975b",
+
+          "warning": "#fcd331",
+
+          "error": "#ef6b8a",
+        },
+      },
+      "business"
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
