20ce88bc4c22ea9242bfe3a567720270147800b9
Saiful Islam <sakibbhaiya143@gmail.com>
2023-07-18 17:40:29 +0600

    Countdown added

--- src/Pages/Home/HotDeals/HotDeals.jsx
+++ src/Pages/Home/HotDeals/HotDeals.jsx
@@ -1,7 +1,48 @@
 import moment from 'moment/moment';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const HotDeals = () => {
+
+    const [countDays , setCountDays] = useState(0);
+    const [countHours  , setCountHours] = useState(0);
+    const [countMinutes , setCountMinutes] = useState(0);
+    const [countSeconds  , setCountSeconds] = useState(0);
+
+
+    useEffect(()=>{
+        const endDate = new Date("2023-07-26").getTime();
+
+            // Update the countdown every second
+    const countdownTimer = setInterval(() => {
+        // Get the current date and time
+        const now = new Date().getTime();
+  
+        // Calculate the time remaining
+        const distance = endDate - now;
+  
+        // Calculate days, hours, minutes, and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+        // Update the countdown state
+        setCountSeconds(seconds);
+        setCountMinutes(minutes);
+        setCountHours(hours);
+        setCountDays(days)
+  
+        // If the countdown is over, clear the timer
+        if (distance < 0) {
+          clearInterval(countdownTimer);
+        }
+      }, 1000);
+  
+      // Cleanup the timer when the component unmounts
+      return () => clearInterval(countdownTimer);
+
+    },[])
+
     return (
         <section className='flex items-center justify-center'>
 <div>
@@ -16,21 +57,28 @@ const HotDeals = () => {
 
 
     <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white">
-    <span className="countdown font-mono lg:text-5xl">
-      <span style={{"--value":5}}></span>
+  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white lg:w-[66px]">
+    <span className="font-mono lg:text-5xl">
+      <span>{countDays}</span>
     </span>
     days
   </div> 
-  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white">
-    <span className="countdown font-mono lg:text-5xl">
-    <span style={{"--value":10}}></span>    </span>
+  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white lg:w-[66px]">
+    <span className="font-mono lg:text-5xl">
+    <span>{countHours}</span>    </span>
     hours
   </div> 
-  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white">
-    <span className="countdown font-mono lg:text-5xl">
-    <span style={{"--value":50}}></span></span>
+
+  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white lg:w-[66px]">
+    <span className="font-mono lg:text-5xl">
+    <span>{countMinutes}</span></span>
     min
+  </div> 
+
+  <div className="flex flex-col bg-primary h-fit p-2 rounded-md text-center font-bold text-white lg:w-[66px]">
+    <span className="font-mono lg:text-5xl">
+    <span>{countSeconds}</span></span>
+    sec
   </div> 
 </div>
 </div>
